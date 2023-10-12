8e4d9de2f030991fcd980aaf88d16e61b713e3d0
Pagrune <pagrune@orange.fr>
2023-10-12 15:57:14 +0200

    installation npm mysql + modif server BDD

--- index.js
+++ index.js
@@ -1,32 +1,45 @@
-const app = require('express')();
-const http = require('http').Server(app);
-const io = require('socket.io')(http);
+require('dotenv').config();
+const express = require('express');
+const mysql = require('mysql');
 
-io.on('connection', (socket) => {
-    // Gérez les événements ici (nouvelle connexion, messages, déconnexion, etc.)
+const app = express();
+const http = require('http');
+const cors = require('cors');
+const { Server } = require('socket.io');
+
+app.use(cors()); // Add cors middleware
+const server = http.createServer(app);
+
+// MySQL Connection
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'chatv2'
 });
 
-const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'chatv2'
+connection.connect();
+
+// Create an io server and allow for CORS from http://localhost:3000 with GET and POST methods
+const io = new Server(server, {
+    cors: {
+        origin: "http://localhost:3000",
+        methods: ['GET', 'POST'],
+    },
   });
-  
-  connection.connect();
 
 // Add this
 app.get('/', (req, res) => {
     res.send('Hello world');
   });
 
-app.get('/data', (req, res) => {
-    connection.query('SELECT * FROM table_name', (error, results, fields) => {
+app.get('/sujet', (req, res) => {
+    connection.query('SELECT * FROM sujet', (error, results, fields) => {
       if (error) throw error;
       res.json(results);
     });
 });
 
-http.listen(4000, () => {
+server.listen(4000, () => {
     console.log('Serveur en écoute sur le port 4000');
 });
