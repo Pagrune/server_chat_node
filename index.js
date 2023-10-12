3b961d79749cb1119821c9f91bfb9a5f7f003d2a
Pagrune <pagrune@orange.fr>
2023-10-12 14:37:21 +0200

    modification pour import BDD

--- index.js
+++ index.js
@@ -6,6 +6,27 @@ io.on('connection', (socket) => {
     // Gérez les événements ici (nouvelle connexion, messages, déconnexion, etc.)
 });
 
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'chatv2'
+  });
+  
+  connection.connect();
+
+// Add this
+app.get('/', (req, res) => {
+    res.send('Hello world');
+  });
+
+app.get('/data', (req, res) => {
+    connection.query('SELECT * FROM table_name', (error, results, fields) => {
+      if (error) throw error;
+      res.json(results);
+    });
+});
+
 http.listen(4000, () => {
     console.log('Serveur en écoute sur le port 4000');
 });
